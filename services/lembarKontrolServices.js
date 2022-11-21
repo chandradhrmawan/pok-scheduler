cea783b7809597e54bfe0417ef4dc4cd104bdd83
chandra darmawan <chandradarmawan17@gmail.com>
2022-11-21 22:44:24 +0700

    Update lembarKontrolServices.js

--- services/lembarKontrolServices.js
+++ services/lembarKontrolServices.js
@@ -90,8 +90,37 @@ const fixDataLembarKontrolPenRo = async (rawData) => {
                 if (dataVolAkhir1) {
                     rawData[index]['AKHIR_VOL'] = dataVolAkhir1['VAL']
                 }
-            }
 
+                let dataVol1 = await db.query(`select
+                    sat.KDOUTPUT,
+                    sat.KDSOUTPUT,
+                    sat.VOLKEG,
+                    VOLKEG AS VAL,
+                    ucase(sat.SATKEG) AS SATKEG,
+                    LVL 
+                from
+                    pok_online.tr_satuan_3 sat
+                where
+                1=1
+                and sat.SATKEG = SUBSTR('${rawData[index]['SATUAN']}',1,5)	
+                and sat.KDSATKER = '${rawData[index]['KDSATKER']}'
+                and sat.KDOUTPUT = '${KDOUTPUT}'
+                and sat.KDSOUTPUT = '${KDSOUTPUT}'
+                and sat.LVL = '05'
+                group by
+                    sat.KDOUTPUT,
+                    sat.KDSOUTPUT,
+                    sat.KDSATKER,
+                    sat.THANG,
+                    sat.LVL`, {
+                    plain: true,
+                    type: QueryTypes.SELECT,
+                })
+
+                if (dataVol1) {
+                    rawData[index]['AWAL_VOL'] = dataVol1['VAL']
+                }
+            }
             resultData.push(rawData[index])
         }
         return resultData
